0d146b81792f56de90a71770ddb6ed6f4bf59d1a
suresh-peddimsetti-milo <suresh.peddimsetti@miloapp.in>
2026-05-31 10:24:30 +0530

    account rest endoints added

---
+++ api/server.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	db "github.com/suresh/simple_bank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{
+		store: store,
+	}
+
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	server.router = router
+	return server
+}
+
+func (s *Server) Start(address string) error {
+	return s.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{
+		"error": err.Error(),
+	}
+}
