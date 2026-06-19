d7e481af4e3e562f2278ca0ab0a7c783752ca5fc
suresh-peddimsetti-milo <suresh.peddimsetti@miloapp.in>
2026-06-19 11:28:07 +0530

    fix: viper added

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,11 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://suresh:2003@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/suresh/simple_bank/util"
 )
 
 var testQueries *Queries
@@ -20,7 +16,11 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
