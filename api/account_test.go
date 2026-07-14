f01af0cdcc3ad6fb225ef300596a3ea81a443c33
suresh-peddimsetti-milo <suresh.peddimsetti@miloapp.in>
2026-07-14 11:39:09 +0530

    fix: login user api endpoint added

--- api/account_test.go
+++ api/account_test.go
@@ -90,8 +90,7 @@ func TestGetAccountAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
