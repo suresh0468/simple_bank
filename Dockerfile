617acf90a36f4277e2a2ca54e25d5ac236aa5057
suresh-peddimsetti-milo <suresh.peddimsetti@miloapp.in>
2026-08-22 13:08:06 +0530

    start.sh file removed

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,6 @@ RUN go mod download
 
 RUN go build -o main .
 
-RUN apk add curl
-RUN curl -L https://github.com/golang-migrate/migrate/releases/download/v4.17.1/migrate.linux-amd64.tar.gz | tar xvz
 
 # Stage 2
 FROM alpine:3.22
@@ -18,11 +16,9 @@ FROM alpine:3.22
 WORKDIR /app
 
 COPY --from=builder /app/main .
-COPY --from=builder /app/migrate .
 COPY app.env .
-COPY start.sh .
 COPY db/migration ./db/migration
 
 EXPOSE 8081
 CMD ["/app/main"]
-ENTRYPOINT ["/app/start.sh"]
+ENTRYPOINT ["/app/main"]
--- main.go
+++ main.go
@@ -19,6 +19,10 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 //go:embed doc/swagger/*
@@ -34,10 +38,25 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 
 	go runGrpcServer(config, store)
 	runGatewayServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance:", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up:", err)
+	}
+
+	log.Println("db migrated successfully")
 }
 
 func runGrpcServer(config util.Config, store db.Store) {
